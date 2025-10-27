24966f73011f3894d7c62aed8d19b42b092b75d0
luiz-urquiza <85198627+luiz-urquiza@users.noreply.github.com>
2025-10-27 08:53:02 -0400

    Visão de autores utilizando a classe Modal

--- views/autores/autores.php
+++ views/autores/autores.php
@@ -1,5 +1,6 @@
 <?php
     require_once ("./orm/autor.php");
+    require_once ("./classes/auxiliares/modal.php");
 
     $autores = Autor::all();
 ?>
@@ -22,51 +23,31 @@
     <tbody>
         <?php
             foreach ($autores as $autor){
-                echo "<tr>";
-                    echo "<td>{$autor->id}</td>";
-                    echo "<td>{$autor->nome}</td>";
-                    echo "<td>{$autor->pais()->gentilico}</td>";
+              $modal = new Modal($autor->id);
+              $modal->setMensagem("Confirma a exclusão do autor {$autor->nome}!!");
 
-                    echo "<td>";
-                    echo count($autor->livros());
-                    echo "</td>";
+              echo <<<HTML
+                  <tr>
+                      <td>{$autor->id}</td>
+                      <td>{$autor->nome}</td>
+                      <td>{$autor->pais()->gentilico}</td>
+                HTML;
 
-                    echo "<td>";
-                    echo "<div class='btn-group'>";
-                        echo "<a class='btn btn-primary btn-sm'>Alterar</a>"; 
-                        echo "<button type='button' class='btn btn-sm btn-primary' data-bs-toggle='modal' data-bs-target='#excluir_{$autor->id}'>";
-                        echo "Excluir";
-                        echo "</button>";
-                    echo "</div>";
-?>
-                    <!-- The Modal -->
-                    <div class="modal" id="<?php echo "excluir_{$autor->id}"?>">
-                      <div class="modal-dialog">
-                        <div class="modal-content">
-                    
-                          <!-- Modal Header -->
-                          <div class="modal-header">
-                            <h4 class="modal-title">Confirma a exclusão?</h4>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                          </div>
-                    
-                          <!-- Modal body -->
-                          <div class="modal-body">
-                            <?php echo "Excluindo o autor #{$autor->id} - {$autor->nome}" ?>
-                          </div>
-                    
-                          <!-- Modal footer -->
-                          <div class="modal-footer">
-                            <a href="index.php?view=autores/excluir&id=<?php echo $autor->id ?>" class="btn btn-success">Confirmar</a>
-                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                          </div>
-                    
-                        </div>
-                      </div>
-                    </div>                    
-<?php
-                    echo "</td>";
+                echo "<td>";
+                  foreach($autor->livros() as $livro){
+                    echo "$livro->titulo<br>";
+                  }                      
+                echo "</td>";
 
+                echo "<td>";
+                  echo "<div class='btn-group'>";
+                    echo "<a class='btn btn-primary btn-sm' href='#'>Alterar</a>";
+                    $modal->gerarBotao();
+                  echo "</div>";
+
+                  $modal->gerarModal();
+
+                echo "</td>";
                 echo "</tr>";
             }
         ?>
